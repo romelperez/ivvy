24660c9584fc2291ec1a64944921b796718d9016
Romel Perez <ronelprhone@gmail.com>
2023-09-29 00:49:12 -0500

    build: scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts']
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['umd'],
-      name: 'ivvy',
-      fileName: 'ivvy',
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        dir: path.resolve(__dirname, 'build/umd')
-      }
-    }
   }
 })
